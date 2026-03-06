d1cabd275fe15a125002bcb3663aa53869016e65
Artem Krinitsyn <arkriny@arkriny.com>
2026-03-06 19:57:13 +0000

    Add support for Alpine Linux

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"net/url"
 	"os"
 
+	"github.com/arkriny/pkgrep/internal/alpine"
 	"github.com/arkriny/pkgrep/internal/archlinux"
 	"github.com/arkriny/pkgrep/internal/aur"
 	"github.com/arkriny/pkgrep/internal/npm"
@@ -24,6 +25,7 @@ type Repository struct {
 }
 
 var repos = []Repository{
+	{"Alpine", alpine.Query},
 	{"Arch", archlinux.Query},
 	{"AUR", aur.Query},
 	{"NPM", npm.Query},
