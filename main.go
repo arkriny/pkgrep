3ecb0a160a00fcd1081b5acb97cb3b94da805ba3
Artem Krinitsyn <arkriny@arkriny.com>
2026-03-07 00:08:11 +0000

    Add support for Snapcraft

--- main.go
+++ main.go
@@ -21,6 +21,7 @@ import (
 	"github.com/arkriny/pkgrep/internal/opensuse"
 	"github.com/arkriny/pkgrep/internal/pypi"
 	"github.com/arkriny/pkgrep/internal/rubygems"
+	"github.com/arkriny/pkgrep/internal/snapcraft"
 	"github.com/arkriny/pkgrep/internal/ubuntu"
 	"github.com/arkriny/pkgrep/internal/voidlinux"
 )
@@ -49,6 +50,7 @@ var repos = []Repository{
 	{"openSUSE", opensuse.Query},
 	{"PyPI", pypi.Query},
 	{"RubyGems", rubygems.Query},
+	{"Snapcraft", snapcraft.Query},
 	{"Ubuntu", ubuntu.Query},
 	{"Void", voidlinux.Query},
 }
